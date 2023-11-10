7b799d27e4c1c194c2fe32c4ab066d7bd31c3a70
Maniii97 <mjha34499@gmail.com>
2023-11-10 22:44:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/store101/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.store101.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.store101.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/store101/auth/SignUpActivity.kt
@@ -0,0 +1,47 @@
+package com.example.store101.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
+import com.example.store101.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var etName : EditText
+    private lateinit var etEmail : EditText
+    private lateinit var etPassword : EditText
+    private lateinit var etConfirmPassword : EditText
+    private lateinit var etPhone : EditText
+    private lateinit var btnSignUp : AppCompatButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        etName = findViewById(R.id.etName)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        etConfirmPassword = findViewById(R.id.etConfirmPassword)
+        etPhone = findViewById(R.id.etPhone)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            onSignUpClick()
+        }
+    }
+
+    private fun authenticatePassword() : Boolean {
+        val password = etPassword.text.toString()
+        val confirmPassword = etConfirmPassword.text.toString()
+        if(password != confirmPassword){
+            etConfirmPassword.error = "Password does not match"
+            return false
+        }
+        return true
+    }
+
+    private fun onSignUpClick() {
+        TODO("Not yet implemented")
+    }
+}
